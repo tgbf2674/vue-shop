e13635a15366dba9f706bb98ed17371c0eec6917
maqing <136518041@qq.com>
2021-05-03 20:33:22 +0800

    finish report

---
+++ src/components/report/Report.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <!--  面包屑导航区  -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/Home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <div id="main" style="width: 750px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import * as echart from 'echarts'
+import { mergeObject } from '@/utils/mergeObject'
+
+export default {
+  data () {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+
+    }
+  },
+  async mounted () {
+    const myChart = echart.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取折线图数据失败')
+    }
+    const result = mergeObject(res.data, this.options)
+    myChart.setOption(result)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
